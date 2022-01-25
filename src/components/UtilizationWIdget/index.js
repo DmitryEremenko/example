c32fdd14206956f5dadecf6da618b598df2e93b7
dmitry.yeromenko <dmitry.yeromenko>
2022-01-25 13:50:12 +0200

    fix

--- src/components/UtilizationWIdget/index.js
+++ src/components/UtilizationWIdget/index.js
@@ -15,29 +15,27 @@ export const UtilizationWidget = () => {
 
   const utilization = utilizations.reduce((acc, [, value]) => acc + Number(value), 0);
   return (
-    <>
-      <StyledCard title={`UTILIZATION (${utilization})`} bordered>
-        {activeTab === 'bars' ? (
-          utilizations.map(([label, value]) => (
-            <ProgressItem value={value} title={label} key={label} />
-          ))
-        ) : (
-          <>
-            {utilizations.map(([name, value]) => {
-              return (
-                <Row justify='center'>
-                  <Col span={14}>
-                    <Typography.Title level={5}>{LabelCollection[name]}</Typography.Title>
-                  </Col>
-                  <Col span={10}>
-                    <Typography.Text>{value}</Typography.Text>
-                  </Col>
-                </Row>
-              );
-            })}
-          </>
-        )}
-      </StyledCard>
-    </>
+    <StyledCard title={`UTILIZATION (${utilization})`} bordered>
+      {activeTab === 'bars' ? (
+        utilizations.map(([label, value]) => (
+          <ProgressItem value={value} title={label} key={label} />
+        ))
+      ) : (
+        <>
+          {utilizations.map(([name, value]) => {
+            return (
+              <Row justify='center'>
+                <Col span={14}>
+                  <Typography.Title level={5}>{LabelCollection[name]}</Typography.Title>
+                </Col>
+                <Col span={10}>
+                  <Typography.Text>{value}</Typography.Text>
+                </Col>
+              </Row>
+            );
+          })}
+        </>
+      )}
+    </StyledCard>
   );
 };
